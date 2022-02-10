ab4bc379181fb44586117fa66aaf8c206f0269b2
sohan-fahad <sohan.bitspeck@gmail.com>
2022-02-10 09:24:52 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function cardDistribution(arr) {
+    let customer = [];
+    for (let i = 0; i < arr.length; i++) {
+        let districtName = arr[i].district.slice(0, 2).toUpperCase();
+        let currentYear = arr[i].currentYear.toString().substr(-2)
+        let postalCode = arr[i].postNo.toString().slice(0, 2)
+        let birthYear = arr[i].birthYear
+        let concatNumber = districtName + currentYear + postalCode + birthYear
+        let padNumber = concatNumber.padEnd(15, 0)
+        let lastDigit = i + 1
+        let cardNumber = padNumber + lastDigit
+
+        let obj = { cardNumber }
+
+        if (lastDigit % 2 === 0) {
+            obj.gift = 'R'
+        }
+        else {
+            obj.gift = 'W'
+        }
+
+        obj.priority = arr[i].priority
+
+        customer[i] = obj
+    }
+    const sortedCustomer = customer.sort((a, b) => (a.priority > b.priority) ? 1 : -1)
+    return sortedCustomer
+
+}
